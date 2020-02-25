284aa58a6d8581de0fa3b723a6760797a05bea52
Quang Dang <quanghd96@gmail.com>
2020-02-25 17:47:25 +0700

    add marker

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -3,10 +3,33 @@
 </template>
 
 <script lang="ts">
+import GraphicsLayer from 'esri/layers/GraphicsLayer';
+import Graphic from 'esri/Graphic';
+
 export default {
     name: 'WebMap',
     async mounted() {
         const app = await import('../data/map');
+        const graphicsLayer = new GraphicsLayer();
+        app.webmap.add(graphicsLayer);
+        const point = {
+            type: 'point',
+            longitude: 105.8194541,
+            latitude: 21.0227788
+        };
+        const simpleMarkerSymbol = {
+            type: 'picture-marker', // autocasts as new PictureMarkerSymbol()
+            url: 'https://static.arcgis.com/images/Symbols/Shapes/BlackStarLargeB.png',
+            width: '64px',
+            height: '64px'
+        };
+
+        const pointGraphic = new Graphic({
+            geometry: point,
+            symbol: simpleMarkerSymbol
+        });
+
+        graphicsLayer.add(pointGraphic);
         app.initialize(this.$el);
     }
 };
